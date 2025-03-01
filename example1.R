3628a709098bc4cf6ead4ef7c82b58ae78a44d72
Huyanmei123 <96899896+Huyanmei123@users.noreply.github.com>
2025-03-01 14:57:50 +0800

    Update example1.R
    give an example

--- example1.R
+++ example1.R
@@ -1,1 +1,18 @@
+library(mclust)
+source("./R/adaptive.R")
 
+
+data<-read.csv("./goolam/goolam.csv",header = T,sep = ",", row.names = 1)
+data<-as.matrix(data)
+data<-log(data+1)
+
+
+label<-read.csv("./goolam/goolam_label.csv", header = TRUE, row.names = 1)
+ncores <- 8
+seed <- 0
+result <- adaptive(k = NULL, data = data, ncores = ncores, seed = seed)
+
+label <- as.vector(label$cell_type1)
+final <- result$cluster
+
+print(mclust::adjustedRandIndex(final,label))
