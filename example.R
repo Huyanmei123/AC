c2069dbc8cff8e787f29e2e12f0a7e4677bc0298
Huyanmei123 <96899896+Huyanmei123@users.noreply.github.com>
2025-03-01 15:34:46 +0800

    Update example.R

--- example.R
+++ example.R
@@ -8,9 +8,11 @@ data<-log(data+1)
 
 
 label<-read.csv("./goolam/goolam_label.csv", header = TRUE, row.names = 1)
-ncores <- 4
-seed <- 0
-result <- adaptive(k = NULL, data = data, ncores = ncores, seed = seed)
+
+#k: the number of the clusters for spectral clustering, if k is null, it will automatically get a value in the algorithm
+#ncores: the number of workers 
+#seed: spectral clustering
+result <- adaptive(k = NULL, data = data, ncores = 4, seed = 0)
 
 label <- as.vector(label$cell_type1)
 final <- result$cluster
